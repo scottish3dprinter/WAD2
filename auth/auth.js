494e31375afba79354c64cf7e8d8adb13bc54f4a
scottish3dprinter <arron@fdit.co.uk>
2025-08-11 01:22:24 +0100

    updated auth added error handeling

--- auth/auth.js
+++ auth/auth.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcrypt');
+
 const UsersModel = require('../models/userModel');
 const usersModel = new UsersModel();
 
@@ -6,7 +7,7 @@ function loginPage(req, res) {
     res.render('login');
 }
 
-function login(req, res, next) {
+function login(req, res) {
     let username = req.body.username;
     let password = req.body.password;
 
@@ -17,21 +18,22 @@ function login(req, res, next) {
         }
         if (!user) {
             console.log("user ", username, " not found");
+            return res.status(401).redirect('/login');
         }
         bcrypt.compare(password, user.password, function (err, result) {
             if (result) {
                 req.session.user = {
                     username: user.user,
-                     id: user._id,
-                     role: user.level
+                    id: user._id,
+                    role: user.level
                 };
-             next();
-             } else {
+                return res.redirect('/dashbord');
+            } else {
                 return res.render("login");
-             }
+            }
         });
     });
-};
+}
 
 function registerPage(req, res) {
     res.render('register');
@@ -43,6 +45,17 @@ function register(req, res) {
     if (!username || !password) {
         return res.status(400).send();
     }
+
+    userModel.lookup(username, function (err, existingUser) {
+        if (err) {
+            console.error("Error looking up user", error);
+            return res.status(500).send();
+        }
+        if (existingUser) {
+            console.error("User already exists", username);
+            return res.status(500).send();
+        }
+
     usersModel.create(username, password, 2);
     res.redirect('login');
 }
@@ -52,4 +65,4 @@ module.exports = {
     login,
     registerPage,
     register
-}
+};
